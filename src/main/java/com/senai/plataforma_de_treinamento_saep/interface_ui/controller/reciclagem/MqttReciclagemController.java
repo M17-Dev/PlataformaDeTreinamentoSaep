e88314ee39829467b9a94c10ac9cf9969f90ac1b
Raul Miguel C. Magalhães <buguelo246@gmail.com>
2025-11-26 15:26:09 -0300

    Update MqttReciclagemController.java
    arrumando uns erros

--- src/main/java/com/senai/plataforma_de_treinamento_saep/interface_ui/controller/reciclagem/MqttReciclagemController.java
+++ src/main/java/com/senai/plataforma_de_treinamento_saep/interface_ui/controller/reciclagem/MqttReciclagemController.java
@@ -17,25 +17,19 @@ public class MqttReciclagemController {
 
     private final UsuarioTampinhaService tampinhaService;
 
-    /**
-     * Assina o tópico 'topico/reciclagem/entrada' e recebe o payload.
-     * O tópico deve ser o mesmo que o seu dispositivo no Wokwi está publicando.
-     */
-    @MqttSubscriber("topico/reciclagem/entrada")
+    
+    @MqttSubscriber("tampinhas/login")
     public void receberDadosReciclagem(@MqttPayload ReciclagemMqttDTO dadosReciclagem) {
         try {
-            System.out.println("MQTT Recebido no Tópico 'topico/reciclagem/entrada': " + dadosReciclagem);
+            System.out.println("MQTT Recebido no Tópico 'tampinhas/login': " + dadosReciclagem);
 
-            // Chama o serviço para atualizar o saldo do usuário
             tampinhaService.adicionarTampinhas(
                     dadosReciclagem.pin(),
                     dadosReciclagem.quantidadeTampinhas()
             );
 
         } catch (Exception e) {
-            // É importante logar erros no listener MQTT, pois eles não serão visíveis em uma requisição HTTP.
             System.err.println("Erro ao processar mensagem MQTT: " + e.getMessage());
-            // Você pode optar por notificar o sistema ou re-tentar dependendo da regra de negócio
         }
     }
 }
